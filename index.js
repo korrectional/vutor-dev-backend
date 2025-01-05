33e242f6fdf130d36a7e566cab14009e87ba1631
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-01-04 19:10:47 -0500

    Test

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { serve } = require('@hono/node-server')
 const { Hono } = require('hono')
 
 const app = new Hono()
-app.get('/api', (c) => c.text('Hono meets Node.js'))
+app.get('/api', (c) => {
+  return c.json({ message: 'Server connected' })
+})
 
 serve(app, (info) => {
   console.log('Listening on http://localhost:3000') // Listening on http://localhost:3000
