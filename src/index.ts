31d4fabf633b090d5c794b2879980ceafd56f13a
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-01-05 11:48:14 -0500

    Change to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 const { serve } = require('@hono/node-server')
 const { Hono } = require('hono')
+const { cors } = require('hono/cors')
 
 const app = new Hono()
+
+app.use( // allowing this adress to send requests to our server
+  '/*',
+  cors({
+    origin: ['http://localhost:5173'],
+  })
+)
+
+
 app.get('/api', (c) => {
   return c.json({ message: 'Server connected' })
 })
