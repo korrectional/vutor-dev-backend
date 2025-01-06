878c5b462b2a89697d30cc2476fd4e4632d2b378
Arpan122 <aniruddha.arpan12@gmail.com>
2025-01-05 23:28:45 -0500

    Installed socket, moved server code in prep for websockets

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
-const { serve } = require('@hono/node-server')
-const { Hono } = require('hono')
-const { cors } = require('hono/cors')
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors'
+import { Server } from 'socket.io'
 
-const app = new Hono()
 
-app.use( // allowing this adress to send requests to our server
+const app = new Hono();
+const httpServer = serve({
+    fetch: app.fetch,
+    port: 3000
+}, (info) => {
+    console.log('Listening on http://localhost:3000') // Listening on http://localhost:3000
+});
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use( // allowing this address to send requests to our server
   '/*',
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:3000'],
   })
-)
+);
 
 
 app.get('/api', (c) => {
-  return c.json({ message: 'Server connected' })
-})
+  return c.json({ message: 'Server connected' });
+});
 
-serve(app, (info) => {
-  console.log('Listening on http://localhost:3000') // Listening on http://localhost:3000
+app.get ('/', (c) => {
+    console.log('Hello World');
+    return c.html('<h1> Hello World </h1>');
 })
