b436eb8de4cb66cb725b1f683fdfd74f13f1863c
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-01-13 21:18:44 -0500

    Sign In placeholder

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 const { serve } = require('@hono/node-server')
 const { Hono } = require('hono')
 const { cors } = require('hono/cors')
-
+const fs = require('fs')
 const app = new Hono()
 
 app.use( // allowing this adress to send requests to our server
   '/*',
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://143e-75-52-101-39.ngrok-free.app','http://localhost:3000'],
   })
 )
 
@@ -16,6 +16,15 @@ app.get('/api', (c) => {
   return c.json({ message: 'Server connected' })
 })
 
-serve(app, (info) => {
-  console.log('Listening on http://localhost:3000') // Listening on http://localhost:3000
+app.post('/api/signin', async (c) => {
+  return c.json({ message: 'Server connected' })
+})
+
+
+serve({
+  fetch: app.fetch,
+  port: 3001,
+},
+() => {
+  console.log("Listening on http://localhost:3001")
 })
