fed5b28042e4b021077772dc688b3aff769ee60e
Arpan122 <aniruddha.arpan12@gmail.com>
2025-01-14 23:25:14 -0500

    Changed server port to 3000, couldn't fix bug with what looks like missing MongoDB URI

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,28 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { Server } from 'socket.io';
+import bcrypt from 'bcryptjs'
+import { MongoClient } from 'mongodb';
+import { config } from 'dotenv'; // this gets the .env file
 
-const app = new Hono();
+config()
+
+// setup mongodb
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+
+const api = new Hono()
 const PORT = 3000;
 const corsOptions = {
     origin: ["http://localhost:5173"],
 };
 
 //Runs the Server
-app.use("/api/*", cors(corsOptions));
+api.use("/api/*", cors(corsOptions));
 
 const httpServer = serve({
-  fetch: app.fetch,
+  fetch: api.fetch,
   port: PORT
 }, (info) => {
   console.log(`Listening on http://localhost:${PORT}`); // Listening on http://localhost:3001
@@ -21,6 +31,62 @@ const httpServer = serve({
 
 
 //Routes
-app.get('/api', (c) => {  
+async function hashPwd(password: string): Promise<string> {
+  return await bcrypt.hash(password, 10);
+}
+
+api.get('/api', (c) => {  
   return c.json({ message: 'Server connected' });
+});
+
+api.post('/api/signup', async (c) => {
+  const { email, password } = await c.req.json();
+  const database = client.db('voluntorcluster');
+  const users = database.collection('user');
+
+  const exists = await users.findOne({ email: email });
+  if (exists) {
+    return c.json({ message: "User already exists" }, 400);
+  }
+
+  const pass = await hashPwd(password);
+  
+  const newUser = {
+    email,
+    password: pass, // Store hashed password
+    createdAt: new Date(),
+  };
+
+  await users.insertOne(newUser);
+
+  return c.json({ message: "Signup successful" }, 201); 
+});
+
+
+api.post('/api/signin', async (c) => {
+  const { email, password } = await c.req.json();
+  const database = client.db('voluntorcluster');
+  const users = database.collection('user');
+
+  //typechecks for bcript
+  
+  const userEmail = await users.findOne({ email: email });
+  if (!userEmail) {
+    console.error("INCORRECT EMAIL");
+    return c.json({ message: "Incorrect email" }, 400); 
+  }
+  
+  if (typeof userEmail.password !== 'string' || typeof password !== 'string') {
+    console.log(typeof userEmail.password)
+    console.log(typeof password)
+    return c.json({ message: "Email and password must be strings." }, 400);
+  }
+  const isPasswordValid = await bcrypt.compare(password, userEmail.password);
+  if (!isPasswordValid) {
+    console.error("INCORRECT PASSWORD");
+    return c.json({ message: "Incorrect password" }, 400); 
+  }
+
+  console.log("EMAIL AND PASSWORD CORRECT");
+  return c.json({ message: "Login successful" }, 200);
 });
